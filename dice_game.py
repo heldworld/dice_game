01bfd17d4c677dc7bc1c516dca354fa15d42856b
heldworld <38360972+heldworld@users.noreply.github.com>
2026-02-16 00:21:29 +0100

    dice_game.py

--- dice_game.py
+++ dice_game.py
@@ -1,8 +1,11 @@
+#import the random module
 import random
+
 print("Hello!")
 while True:
     print(" May i know your name?: ")
     userName=input().strip()
+    #Ensure the name is neither empty nor contains a number
     if userName== "":
         print("You need to enter your Name")
         continue
@@ -11,18 +14,22 @@ while True:
         continue
     else:
         break
+
 print ("Hello " + userName + "!," + " welcome to my Dice game")
+#Computer takes a random number
 secretNumber = random.randint(1,6)
 print ("i just rolled a dice")
+
+#only 3 chances to guess
 guessNo=0
-for guessChance in range(1,4):
+for guessChance in range(1,4): 
     guessNo+=1
     try:
         guess=int(input("take a guess: "))
         if guess < 0:
             print ("That cant be a dice")
             continue
-        elif guess == "":
+        elif guess == "": #No empty guess allowed
             print ("you need to enter a number")
             continue
     except ValueError:
@@ -34,6 +41,8 @@ for guessChance in range(1,4):
         print ("You guessed higher")
     else:
         break
+    
+    #output when it is guessed on first attempt is a bit different
 if guessNo==1 and guess==secretNumber:
         print("Congrats " + userName + "!," + " You guessed right after " + str(guessNo) + " try" )
 elif guess == secretNumber:
