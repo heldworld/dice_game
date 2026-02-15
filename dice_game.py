e9774a712e52f69fdf1e8f6f17f2f7b823622056
heldworld <38360972+heldworld@users.noreply.github.com>
2026-02-15 23:59:16 +0100

    Add files via upload

---
+++ dice_game.py
@@ -0,0 +1,42 @@
+import random
+print("Hello!")
+while True:
+    print(" May i know your name?: ")
+    userName=input().strip()
+    if userName== "":
+        print("You need to enter your Name")
+        continue
+    elif not userName.isalpha():
+        print ("alphabets only allowed")
+        continue
+    else:
+        break
+print ("Hello " + userName + "!," + " welcome to my Dice game")
+secretNumber = random.randint(1,6)
+print ("i just rolled a dice")
+guessNo=0
+for guessChance in range(1,4):
+    guessNo+=1
+    try:
+        guess=int(input("take a guess: "))
+        if guess < 0:
+            print ("That cant be a dice")
+            continue
+        elif guess == "":
+            print ("you need to enter a number")
+            continue
+    except ValueError:
+        print ("You need to enter a number")
+        continue
+    if guess < secretNumber:
+        print ("Oops... that guess is too low")
+    elif guess > secretNumber:
+        print ("You guessed higher")
+    else:
+        break
+if guessNo==1 and guess==secretNumber:
+        print("Congrats " + userName + "!," + " You guessed right after " + str(guessNo) + " try" )
+elif guess == secretNumber:
+    print("Congrats " + userName + "!," + " You guessed right after " + str(guessNo) + " tries" )
+else:
+    print("You exhausted the available chances, the number is: " + str(secretNumber))
